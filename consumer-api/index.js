c2414c824f360d520e9d9b1c8c8571094e00a795
RealKareemAnis <karimov_off_devacc@outlook.com.com>
2024-02-03 12:09:18 +0200

    fix shit

--- consumer-api/index.js
+++ consumer-api/index.js
@@ -1,3 +1,5 @@
+const handle_home_page = require("./handle_home_page/handle_home_page");
+
 const log = console.log;
 
 const http = require("http");
@@ -6,26 +8,15 @@ const path = require("path");
 const env = require("dotenv");
 env.config({ path: path.join("configs", "network.env") });
 
-const router = http.createServer((req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  const path = url.pathname;
-  const method = req.method;
+const router = http.createServer(async (req, res) => {
+  try {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const PATH = url.pathname;
+    const METHOD = req.method;
 
-  if (path === "/") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        data: " /",
-      })
-    );
-  }
-  if (path === "/x") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        data: " /x",
-      })
-    );
+    PATH === "/homepage" ? handle_home_page(req, res) : res.end("error 404");
+  } catch (error) {
+    res.end("server error", error);
   }
 });
 
@@ -33,3 +24,8 @@ const IP = process.env.IP;
 const PORT = process.env.PORT;
 
 router.listen(PORT, IP);
+router.on("listening", () => {
+  setTimeout(() => {
+    log("router listening on port:", PORT, ", socket:", IP);
+  }, 0);
+});
---
+++ consumer-api/routes.md
@@ -0,0 +1,3 @@
+## /homepage
+
+### for homepage and just sends empty html for now
